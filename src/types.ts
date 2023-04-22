c93427495f49dc5866992a7cfe9b9298fea4219d
XLor <yjl9903@vip.qq.com>
2023-04-22 20:24:08 +0800

    docs: add type comment

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,36 @@
 export interface SeaOptions {
+  /**
+   * The output binary name. By default it is inferred from your package.json.
+   */
   binary: string;
 
+  /**
+   * The entry point script path of your application. By default it is inferred from your package.json.
+   */
   main: string;
 
+  /**
+   * Node binary path.
+   *
+   * You should provide a node executable with version higher than 20.0.0.
+   */
   node: string;
 
+  /**
+   * Binary output directory.
+   *
+   * By default it is the same as unbuild context.
+   */
   outDir: string;
 
+  /**
+   * Enable sign on macOS or Windows.
+   */
   sign: boolean;
 
+  /**
+   * Postject configuration.
+   */
   postject: {
     machoSegmentName: string | undefined;
     overwrite: boolean;
